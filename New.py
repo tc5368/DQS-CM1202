3da952f03f752e5d2fae509603fd008fae8c8efc
Tom <tc536890"gmail.com>
2018-11-06 10:17:15 +0000

    First Commit

---
+++ New.py
@@ -0,0 +1,19 @@
+
+
+class student():
+	"""docstring for student"""
+	def __init__(self, ID, name, nickname, classID):
+		self.ID = ID
+		self.name = name
+		self.nickname = nickname
+		self.className = className
+
+class teacher():
+	"""docstring for teacher"""
+	def __init__(self, name, className, classID):
+		super(teacher, self).__init__()
+		self.name = name
+		self.className = className
+		self.classID = classID
+		
+
